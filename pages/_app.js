8c194e1db191395068f85c55937d7b6099b4689c
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-24 15:56:08 -0300

    title change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout'
 import '../pages/test/index.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>CRYPTO~CAT</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>)
 }

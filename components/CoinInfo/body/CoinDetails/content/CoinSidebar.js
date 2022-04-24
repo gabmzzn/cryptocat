4d74e056a4c26f9258ac3c53c775eb88be001a8e
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-24 13:06:11 -0300

    lets try

--- components/CoinInfo/body/CoinDetails/content/CoinSidebar.js
+++ components/CoinInfo/body/CoinDetails/content/CoinSidebar.js
@@ -16,13 +16,13 @@ export default function CoinSidebar(props) {
       <table>
         <tbody>
           <tr>
-            <td><Image width={30} height={30} src={`https://www.cryptocompare.com/${imageURL}`} />
+            <td><Image width={30} height={30} src={`https://www.cryptocompare.com/${imageURL}`} priority={true} alt='' />
               {symbol}
             </td>
             <td>1</td>
           </tr>
           <tr>
-            <td><Image width={30} height={30} src={'https://www.cryptocompare.com/'} />
+            <td><Image width={30} height={30} src={'https://www.cryptocompare.com/'} priority={true} alt='' />
               {selCurrencyToCompare}</td>
             <td>${price}</td>
           </tr>

ca3dd6fda4256bb15f4dc5105bb0f7b0969a0bd7
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-25 18:56:35 -0300

    scaled

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
         <title>CRYPTO~CAT</title>
         <link rel="prefetch" as="image" href="/logo.svg"></link>
         <link rel="prefetch" as="image" href="/icon.svg"></link>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=0.6, width=device-width" />
       </Head>
       <Component {...pageProps} />
     </Layout>)

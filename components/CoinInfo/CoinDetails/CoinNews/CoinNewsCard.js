b15f7eac4706e993bfca02855a71d08a53a3a472
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-26 21:23:11 -0300

    news

--- components/CoinInfo/CoinDetails/CoinNews/CoinNewsCard.js
+++ components/CoinInfo/CoinDetails/CoinNews/CoinNewsCard.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import style from './NewsCard.module.css'
+import style from './CoinNewsCard.module.css'
 export default function NewsCard(props) {
 
     const news = props.data

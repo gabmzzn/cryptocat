3ee6f6bec33a01835e91e9ac70b1e91e55421103
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-27 18:17:29 -0300

    !

--- components/Exhanges/Table/ExhangesTable.js
+++ components/Exhanges/Table/ExhangesTable.js
@@ -1,4 +1,4 @@
-import style from './exchanges.module.css'
+import style from './Exchanges.module.css'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'

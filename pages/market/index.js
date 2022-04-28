5cd101f689781838d898d1839dbe09340c25e91e
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-28 02:27:32 -0300

    better limit

--- pages/market/index.js
+++ pages/market/index.js
@@ -14,7 +14,7 @@ export default function Market() {
             const date = (new Date()).toString()
             const timeLastHour = Date.parse((date.substr(0, 18) + ':00:00' + date.substr(24))) / 1000
 
-            const URL = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=100&tsym=USD'
+            const URL = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=32&tsym=USD'
             const fetchedData = await fetch(URL).then(r => r.json())
             const data = fetchedData.Data.reduce((result, crypto, index) => {
                 if ('RAW' in crypto) {
@@ -41,7 +41,7 @@ export default function Market() {
                     })
                 }
                 return result
-            }, []).slice(0, 50)
+            }, [])
 
             setCurrencyData(data)
             getLiveData(data)

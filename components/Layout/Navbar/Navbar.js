e6ce6e553c7f9a4e1f839b06908af38dc921d463
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-04-30 21:29:15 -0300

    asd

--- components/Layout/Navbar/Navbar.js
+++ components/Layout/Navbar/Navbar.js
@@ -14,14 +14,13 @@ import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
 export default function NavBar() {
 
 	const [scroll, setScroll] = useState(false)
+	const { logo, navbar, navbarShadow, content, hamburgerButton, button, buttonActive, hamburguer, navbarItems } = scss
 
 	useEffect(() => {
 		window.addEventListener("scroll", () => {
 			setScroll(window.scrollY > 5)
 		})
 	}, [])
-
-	const { logo, navbar, navbarShadow, content, hamburgerButton, button, buttonActive, hamburguer, navbarItems } = scss
 
 	const [drawer, setDrawer] = useState(false)
 	const toggleDrawer = (open) => () => {
@@ -47,57 +46,55 @@ export default function NavBar() {
 			</Link>)
 	})
 
+	const home = (
+		<Link href='/' passHref>
+			<div className={logo}>
+				<span>CRYPTO</span>
+				<img src='/icon.webp' height={30} width={30} alt='' />
+				<span>CAT</span>
+			</div>
+		</Link>)
+
 	return (
 		<div className={cx(navbar, { [navbarShadow]: scroll })}>
+			{/* Hamburger Menu */}
+			<div className={hamburguer}>
+				<Button className={hamburgerButton} onClick={toggleDrawer(true)}>
+					<MenuRoundedIcon fontSize="large" />
+				</Button>
+				<Drawer
+					anchor={'left'}
+					open={drawer}
+					onClose={toggleDrawer(false)}
+					BackdropProps={{ invisible: true }}
+					PaperProps={{
+						sx: {
+							backgroundColor: 'rgba(255, 255, 255, 0.6)',
+							backdropFilter: 'blur(20px)'
+						}
+					}}
+				>
+					<Box
+						sx={{ width: 240 }}
+						role="presentation"
+						onClick={toggleDrawer(false)}
+						onKeyDown={toggleDrawer(false)}
+					>
+						<List style={{ display: 'flex', flexDirection: 'column' }}>
+							<div style={{ margin: '10px 0' }}>
+								{home}
+							</div>
+							<Divider style={{ margin: '10px 0' }} />
+							{pages}
+						</List>
+					</Box>
+				</Drawer>
+			</div>
 			<div className={content}>
-				<Link href='/' passHref>
-					<div className={logo}>
-						<span>CRYPTO</span>
-						<img src='/icon.webp' height={30} width={30} alt='' />
-						<span>CAT</span>
-					</div>
-				</Link>
+				{home}
 				<div>
 					<div className={navbarItems}>
 						{pages}
-					</div>
-					<div className={hamburguer}>
-						<Button className={hamburgerButton} onClick={toggleDrawer(true)}>
-							<MenuRoundedIcon fontSize="large" />
-						</Button>
-						<Drawer
-							anchor={'right'}
-							open={drawer}
-							onClose={toggleDrawer(false)}
-							BackdropProps={{ invisible: true }}
-							PaperProps={{
-								sx: {
-									backgroundColor: 'rgba(255, 255, 255, 0.6)',
-									backdropFilter: 'blur(20px)'
-								}
-							}}
-						>
-							<Box
-								sx={{ width: 240 }}
-								role="presentation"
-								onClick={toggleDrawer(false)}
-								onKeyDown={toggleDrawer(false)}
-							>
-								<List style={{ display: 'flex', flexDirection: 'column' }}>
-									<div className={logo} style={{ margin: '10px 0' }}>
-										<Link href='/' passHref>
-											<div className={logo}>
-												<span>CRYPTO</span>
-												<img src='/icon.webp' height={30} width={30} alt='' />
-												<span>CAT</span>
-											</div>
-										</Link>
-									</div>
-									<Divider style={{ margin: '10px 0' }} />
-									{pages}
-								</List>
-							</Box>
-						</Drawer>
 					</div>
 				</div>
 			</div>

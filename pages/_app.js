34c8c6666b132441098ac3ae1f99f399d7903aca
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-05-01 01:12:27 -0300

    try

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>CryptoCat</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no,viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.8, minimum-scale=0.8, maximum-scale=1.0, user-scalable=no,viewport-fit=cover" />
       </Head>
       <Component {...pageProps} />
     </Layout>)

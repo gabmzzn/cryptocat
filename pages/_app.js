f10e3f73c2715bc43348e1fc5d0f1341dec4f570
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-05-01 12:18:10 -0300

    viewport

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=0.8, minimum-scale=0.8, maximum-scale=1.0, user-scalable=no,viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.75, minimum-scale=0.75, maximum-scale=0.75, user-scalable=no,viewport-fit=cover" />
       </Head>
       <Component {...pageProps} />
     </Layout>)

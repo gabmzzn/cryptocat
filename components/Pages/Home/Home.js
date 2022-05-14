618b8a2be8a4ccf56e87dc13b7c0da8cf68a4706
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-05-14 02:34:21 -0300

    something something

--- components/Pages/Home/Home.js
+++ components/Pages/Home/Home.js
@@ -2,10 +2,13 @@ import scss from './Home.module.scss'
 
 export default function Home() {
     return (
-        <div className={scss.logo}>
-            <h1><span>CRYPTO</span>
-                <img src='/catn.webp' alt='Cute Cat' />
-                <span>CAT</span>
-            </h1>
+        <div className={scss.home}>
+            <div className={scss.background}></div>
+            <div className={scss.logo}>
+                <h1><span>CRYPTO</span>
+                    <img src='/catn.webp' alt='Cute Cat' />
+                    <span>CAT</span>
+                </h1>
+            </div>
         </div>)
 }

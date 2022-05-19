3454885f261801c3ba0c5c38c21b80871b67b923
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-05-19 20:39:30 -0300

    asd

--- components/Pages/Exchanges/Exchanges.js
+++ components/Pages/Exchanges/Exchanges.js
@@ -6,20 +6,6 @@ import OutlinedInput from '@mui/material/OutlinedInput'
 import Box from '@mui/material/Box'
 import FormHelperText from '@mui/material/FormHelperText'
 
-function MyFormHelperText() {
-	const { focused } = useFormControl() || {}
-
-	const helperText = React.useMemo(() => {
-		if (focused) {
-			return 'This field is being focused'
-		}
-
-		return 'Helper text'
-	}, [focused])
-
-	return <FormHelperText>{helperText}</FormHelperText>
-}
-
 export default function Exchanges(props) {
 	const { exchanges } = props
 	return (<>
@@ -27,7 +13,6 @@ export default function Exchanges(props) {
 		<Box component="form" noValidate autoComplete="off">
 			<FormControl sx={{ width: '25ch' }}>
 				<OutlinedInput placeholder="Please enter text" />
-				<MyFormHelperText />
 			</FormControl>
 		</Box>
 		<ExchangesTable exchanges={exchanges} />

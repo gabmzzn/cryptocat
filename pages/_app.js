bf453e5b264b6d1b0e5cbed45b580ab9a91da1b4
Juan Gabriel Mazzoleni <51454736+gabmzzn@users.noreply.github.com>
2022-07-11 20:27:12 -0300

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import '../pages/test/index.css'
 import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>CryptoCat</title>
-        <meta name="viewport" content="width=device-width, initial-scale=0.75, minimum-scale=0.75, maximum-scale=0.75, user-scalable=no,viewport-fit=cover" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>)
+	return (
+		<Layout>
+			<Head>
+				<title>CRYPTO😺CAT - Real-time Cryptocurrency Market Information</title>
+				<meta name="viewport" content="width=device-width, initial-scale=0.75, minimum-scale=0.75, maximum-scale=0.75, user-scalable=no,viewport-fit=cover" />
+			</Head>
+			<Component {...pageProps} />
+		</Layout>)
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
-				<title>CRYPTO😺CAT - Real-time Cryptocurrency Market Information</title>
 				<meta name="title" content="CRYPTO😺CAT - Real-time Cryptocurrency Market Information" />
 				<meta name="description" content="Find out the latest prices, general information, and news, of the cryptocurrency world!" />
 
